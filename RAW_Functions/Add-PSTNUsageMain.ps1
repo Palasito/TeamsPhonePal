1a351b3e84be458e9ca405181d7f36d505659414
Palasito <chris.palavouzis@gmail.com>
2022-05-11 17:21:21 +0300

    PSTNUSAGE

--- RAW_Functions/Add-PSTNUsageMain.ps1
+++ RAW_Functions/Add-PSTNUsageMain.ps1
@@ -4,6 +4,9 @@ function Add-PSTNUsageMain {
         $MainCountry
     )
 
-    Set-CsOnlinePstnUsage -Identity Global -Usage @{add="$MainCountry"}
+    Set-CsOnlinePstnUsage -Identity Global -Usage @{add="$($MainCountry)" + "LandlinesOnly"}
+    Set-CsOnlinePstnUsage -Identity Global -Usage @{add="$($MainCountry)" + "LocalOnly"}
+    Set-CsOnlinePstnUsage -Identity Global -Usage @{add="$($MainCountry)" + "LocalOnly_Paid"}
+    Set-CsOnlinePstnUsage -Identity Global -Usage @{add="GeneralInternational"}
     
 }
