6d4b998547342b8edc816767846acec7faad9095
Palasito <chris.palavouzis@gmail.com>
2022-06-27 15:32:23 +0300

    Prolly finished phase 1

--- Module/TeamsPhonePal/Functions/TeamsDeployment.ps1
+++ Module/TeamsPhonePal/Functions/TeamsDeployment.ps1
@@ -118,63 +118,6 @@ function TeamsDeployment {
     }
     #EndRegion
 
-
-
-
-
-
-
-
-
-
-
-
-
-    $confirmation = Read-Host "Do you want to create a set of rules for another Country? [y/n]"
-    if ($confirmation -eq 'y') {
-        do {
-            [string]$NC = Read-Host "Specify new country of Usage using ISO Code or Country Name"
-            [string]$NS = Read-Host "Specify the SBC FQDN"
-            [string]$NP = Read-Host "Specify signalling port of SBC"
-            [string]$L = Read-host "Provide a test Landline phone for validation (excluding the country code eg. 2111122345)"
-            [string]$M = Read-Host "Provide a test Mobile phone for validation (excluding the country code eg. 6911223456)"
-
-            $checkSBC = Get-CsOnlinePSTNGateway -Identity $SBCFQDN -ErrorAction SilentlyContinue
-            if ($null -eq $checkSBC) {
-                try {
-                    $null = SBCConf -FQDN $NS -Port $NP
-                    Write-host "Creation of SBC with FQDN: $($NS) was successful" -ForegroundColor Green
-                }
-                catch {
-                    Write-Host "SBC creation failed. Please check whether the FQDN provided is valid" -ForegroundColor Red
-                    break
-                }
-            }
-
-            try {
-                $null = TelDep =Country $NC -SBCFQDN $NS -Land $L -Mob $M
-                Write-host "Telephony Rules were created successfully" -ForegroundColor Green
-            }
-            catch {
-
-                Write-host "SBC creation failed. Please check whether the FQDN provided is valid" -ForegroundColor Red
-            }
-
-        }
-        until ($confirmation -eq "n")
-    }
-    #endregion
-
-
-
-    #region Variables that need user definition
-    # [string]$MC = Read-Host "Specify Main country of Usage using ISO Code or Country Name"
-    # [string]$SBCFQDN = Read-Host "Specify the SBC FQDN"
-    # [string]$Port = Read-Host "Specify signalling port of SBC"
-    # [string]$Land = Read-host "Provide a test Landline phone for validation (excluding the country code eg. 2111122345)"
-    # [string]$Mob = Read-Host "Provide a test Mobile phone for validation (excluding the country code eg. 6911223456)"
-    #endregion
-    
     # $confirmation = Read-Host "Text Here! [y/n]"
     # if ($confirmation -eq 'n') {
     #     # Do Nothing !
